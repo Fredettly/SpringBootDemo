aef3adf11e28ebf9d3da57af8d6775461f0ca84a
Fredettly <794760487@qq.com>
2020-01-08 21:15:47 +0800

    echo user

--- src/main/java/com/fredettly/demo/controller/AuthorizeController.java
+++ src/main/java/com/fredettly/demo/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Created by xwx_ on 2020/1/7
  */
@@ -22,9 +24,11 @@ public class AuthorizeController {
     private String ClientSecret;
     @Value("${github.Redirect_uri}")
     private String RedirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(ClientId);
         accessTokenDTO.setClient_secret(ClientSecret);
@@ -33,7 +37,14 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if (user != null) {
+            request.getSession().setAttribute("user", user);
+            //登录成功，写session&cookie
+            return "redirect:/";
+        } else {
+            //登录失败,重新登录
+            return "redirect:/";
+        }
+
     }
 }
