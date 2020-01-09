026cd19ad4fe77fa2f58ff31db2c0bb444def5d5
Fredettly <794760487@qq.com>
2020-01-09 21:28:05 +0800

    add h2 and mybatis

---
+++ src/main/java/com/fredettly/demo/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.fredettly.demo.mapper;
+
+/**
+ * Created by xwx_ on 2020/1/9
+ */
+public class UserMapper {
+}
---
+++ src/main/java/com/fredettly/demo/model/User.java
@@ -0,0 +1,7 @@
+package com.fredettly.demo.model;
+
+/**
+ * Created by xwx_ on 2020/1/9
+ */
+public class User {
+}
