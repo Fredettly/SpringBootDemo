27c5a9c2e4ece2bc38f24a4682f51800ca268165
Fredettly <794760487@qq.com>
2020-01-09 21:29:33 +0800

    add db and h2

--- src/main/java/com/fredettly/demo/controller/AuthorizeController.java
+++ src/main/java/com/fredettly/demo/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.fredettly.demo.controller;
 
 import com.fredettly.demo.dto.AccessTokenDTO;
 import com.fredettly.demo.dto.GithubUser;
+import com.fredettly.demo.mapper.UserMapper;
+import com.fredettly.demo.model.User;
 import com.fredettly.demo.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 /**
  * Created by xwx_ on 2020/1/7
@@ -25,6 +28,9 @@ public class AuthorizeController {
     @Value("${github.Redirect_uri}")
     private String RedirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
@@ -36,8 +42,15 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(RedirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        if (user != null) {
+        GithubUser githubuser = githubProvider.getUser(accessToken);
+        if (githubuser != null) {
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubuser.getName());
+            user.setAccountId(String.valueOf(githubuser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
             request.getSession().setAttribute("user", user);
             //登录成功，写session&cookie
             return "redirect:/";
--- src/main/java/com/fredettly/demo/mapper/UserMapper.java
+++ src/main/java/com/fredettly/demo/mapper/UserMapper.java
@@ -1,7 +1,15 @@
 package com.fredettly.demo.mapper;
 
+import com.fredettly.demo.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+
 /**
  * Created by xwx_ on 2020/1/9
  */
-public class UserMapper {
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
 }
