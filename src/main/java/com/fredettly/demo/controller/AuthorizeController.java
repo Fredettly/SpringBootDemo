5e05c174b0857784857a5516a82a4c138f05722d
Fredettly <794760487@qq.com>
2020-01-13 21:39:40 +0800

    insert avatar_url

--- src/main/java/com/fredettly/demo/controller/AuthorizeController.java
+++ src/main/java/com/fredettly/demo/controller/AuthorizeController.java
@@ -52,6 +52,7 @@ public class AuthorizeController {
             user.setAccountId(String.valueOf(githubuser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
+            user.setAvatarUrl(githubuser.getAvatar_url());
             userMapper.insert(user);
             response.addCookie(new Cookie("token" , token));
             return "redirect:/";
--- src/main/java/com/fredettly/demo/dto/GithubUser.java
+++ src/main/java/com/fredettly/demo/dto/GithubUser.java
@@ -1,34 +1,14 @@
 package com.fredettly.demo.dto;
 
+import lombok.Data;
+
 /**
  * Created by xwx_ on 2020/1/7
  */
+@Data
 public class GithubUser {
     private String name;
     private String id;
     private String bio;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
+    private String avatar_url;
 }
