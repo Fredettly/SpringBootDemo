829e6ad4475ed608723493d013092ab083452e72
Fredettly <794760487@qq.com>
2020-01-29 18:04:44 +0800

    add delete

--- src/main/java/com/fredettly/demo/controller/QuestionController.java
+++ src/main/java/com/fredettly/demo/controller/QuestionController.java
@@ -2,6 +2,7 @@ package com.fredettly.demo.controller;
 
 import com.fredettly.demo.dto.QuestionDTO;
 import com.fredettly.demo.mapper.QuestionMapper;
+import com.fredettly.demo.model.Question;
 import com.fredettly.demo.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,13 +16,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class QuestionController {
     @Autowired
+    private QuestionMapper questionMapper;
+    @Autowired
     private QuestionService questionService;
 
     @GetMapping("/question/{id}")
     public String question(@PathVariable(name = "id") Integer id,
                            Model model) {
         QuestionDTO questionDTO = questionService.getById(id);
-        model.addAttribute("question" , questionDTO);
+        model.addAttribute("question", questionDTO);
         return "question";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable(name = "id") Integer id
+    ) {
+        questionMapper.delete(id);
+        return "redirect:/";
     }
 }
