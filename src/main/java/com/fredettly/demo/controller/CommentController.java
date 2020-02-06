cffdeec1169c1cb210accdb141eac7abd9302965
Fredettly <794760487@qq.com>
2020-02-06 22:25:07 +0800

    comment function

---
+++ src/main/java/com/fredettly/demo/controller/CommentController.java
@@ -0,0 +1,34 @@
+package com.fredettly.demo.controller;
+
+import com.fredettly.demo.dto.CommentDTO;
+import com.fredettly.demo.mapper.CommentMapper;
+import com.fredettly.demo.model.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by xwx_ on 2020/2/6
+ */
+@Controller
+public class CommentController {
+    @Autowired
+    private CommentMapper commentMapper;
+
+    @RequestMapping(value = "/comment",method = RequestMethod.POST)
+    public Object post(@RequestBody CommentDTO commentDTO){
+
+        Comment comment = new Comment();
+        comment.setParentId(commentDTO.getParentId());
+        comment.setContent(commentDTO.getContent());
+        comment.setType(commentDTO.getType());
+        comment.setGmtCreate(System.currentTimeMillis());
+        comment.setGmtModified(System.currentTimeMillis());
+        comment.setCommentator(1);
+        commentMapper.insert(comment);
+        return null;
+
+    }
+}
---
+++ src/main/resources/db/migration/V5__Create_comment_table.sql
@@ -0,0 +1,11 @@
+  CREATE TABLE comment
+(
+    id bigint AUTO_INCREMENT PRIMARY KEY,
+    parent_id bigint NOT NULL,
+    content varchar(1024) NOT NULL,
+    type int NOT NULL,
+    commentator int NOT NULL,
+    gmt_create bigint NOT NULL,
+    gmt_modified bigint NOT NULL,
+    like_count bigint DEFAULT 0
+);
